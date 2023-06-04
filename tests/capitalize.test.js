0a0990a20a012b7d224ce8888e464586a78acd85
garlend <garlend123@gmail.com>
2023-06-04 13:53:38 +0300

    update

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,6 +1,3 @@
-// нажмите кнопку run чтобы запустить тесты
-// попробуйте изменять код функции / тестов, запуская проверки заново
-
 import { capitalize } from '../src/capitalize.js';
 
 if (capitalize('hello') !== 'Hello') {
